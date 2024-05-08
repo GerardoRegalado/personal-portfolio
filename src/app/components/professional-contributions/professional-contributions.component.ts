4063f31fffdf4db7d5f370ca8f536bfb17b52e2d
Gerardo Regalado <gerardo.regalado24@gmail.com>
2024-05-07 18:35:42 -0600

    Feat(): Contributions module done

--- src/app/components/professional-contributions/professional-contributions.component.ts
+++ src/app/components/professional-contributions/professional-contributions.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+interface Contributions {
+  company: string;
+  position: string;
+  yearOfStart: number;
+  link: string;
+  location: string;
+  active: boolean;
+}
+
 @Component({
   selector: 'port-professional-contributions',
   templateUrl: './professional-contributions.component.html',
   styleUrl: './professional-contributions.component.scss'
 })
 export class ProfessionalContributionsComponent {
+public professional_contributions: Contributions[] = [
+  {
+    company: 'ASCENDYNAMICS',
+    position: 'Frontend Developer',
+    yearOfStart: 2024,
+    link: 'https://ascendynamics.org',
+    location: 'Chicago, US',
+    active: true
+  },
+  {
+    company: 'OneCarNow',
+    position: 'Frontend Developer Junior',
+    yearOfStart: 2021,
+    link: 'https://www.onecarnow.com',
+    location: 'Mexico City',
+    active: false
+  },
+  {
+    company: 'IronHack',
+    position: 'Fullstack Developer Junior',
+    yearOfStart: 2021,
+    link: 'https://www.ironhack.com/es',
+    location: 'Mexico City',
+    active: false
+  },
+]
 
+public openLink(link: string) {
+  window.open(link, '_blank')
+}
 }
